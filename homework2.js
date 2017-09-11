a165f2c1f110269d3be1f869d99d427de53c5e71
davidbabayan <david.babayan@hotmail.com>
2017-09-11 21:11:47 +0400

    Second commit

---
+++ homework2.js
@@ -0,0 +1,8 @@
+
+var first = "I require";
+var second = " a high grade!";
+var third = "Pleaaaaaaaaase?";
+
+console.log(first + second + "\t" + third);
+
+console.log("At least " + (35 / 10) * 8 + " points!");
